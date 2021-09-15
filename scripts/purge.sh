174f2d556ef9c4d605c79ad0e8f5c532415f2a7b
Zsolt Alfoldi <alfonzso@gmail.com>
2021-09-15 02:12:48 +0200

    .

--- scripts/purge.sh
+++
@@ -1,8 +0,0 @@
-gcloud iam roles delete kubeip --project $PROJECT_ID
-gcloud iam service-accounts delete kubeip-service-account@$PROJECT_ID.iam.gserviceaccount.com
-
-number_of_vm=10
-for i in $(seq 1 $number_of_vm);
-do
-  gcloud compute addresses delete kubeip-ip$i --project=$PROJECT_ID --region=$GCP_REGION --quiet
-done
